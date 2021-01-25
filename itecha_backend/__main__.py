af3e2c99bbcda1a355f3749b908d29b583dabcae
Long <longsiyu@megvii.com>
2021-01-25 10:38:44 +0800

    Rename backend package

--- itecha_backend/__main__.py
+++ itecha_backend/__main__.py
@@ -4,8 +4,24 @@ import traceback
 from io import StringIO
 
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120))
+
+
+class Problem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
 
 PROBLEMS = [
     {"id": 1, "title": "A+B problem", "content": "输入两个数a和b，输出他们的和"},
