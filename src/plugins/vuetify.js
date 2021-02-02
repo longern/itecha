1657ec68d415af2eec84a090aa3db027879b92d7
longern <i@longern.com>
2021-02-02 08:28:01 +0800

    Revert language config

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,12 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import zhHans from 'vuetify/es5/locale/zh-Hans'
 
 Vue.use(Vuetify);
 
-// https://github.com/vuetifyjs/vuetify/issues/4861
-export default new Vuetify();
+export default new Vuetify({
+  lang: {
+    locales: { zhHans },
+    current: 'zhHans',
+  }
+});
