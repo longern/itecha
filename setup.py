42c34e789fab23649359a4c9353f2bd211e693c5
Long <longsiyu@megvii.com>
2021-02-24 11:28:24 +0800

    Deploy to Aliyun FC

--- setup.py
+++ setup.py
@@ -1,7 +1,39 @@
 #!/usr/bin/env python3
-from setuptools import setup, find_packages
+import distutils.cmd
+import os
+import re
+
+import fc2
+from setuptools import find_packages, setup
 
 from itecha import __version__
+
+
+class DeployCommand(distutils.cmd.Command):
+    user_options = []
+
+    def initialize_options(self):
+        """Set default values for options."""
+        pass
+
+    def finalize_options(self):
+        """Post-process options."""
+        pass
+
+    def run(self) -> None:
+        function_compute_arn = os.getenv("FUNCTION_COMPUTE_ARN")
+        assert function_compute_arn
+        match_pattern = r"^acs:fc:([^:]*):(\d+):services/([^.]*)\..*/functions/(.*)$"
+        service_site, account_id, service_name, function_name = re.match(match_pattern, function_compute_arn).groups()
+
+        client = fc2.Client(
+            endpoint=f"http://{account_id}.{service_site}.fc.aliyuncs.com",
+            accessKeyID=os.getenv("ACCESS_KEY_ID"),
+            accessKeySecret=os.getenv("SECRET_ACCESS_KEY"),
+            Timeout=300,
+        )
+        client.update_function(service_name, function_name, codeDir="dist/fc")
+
 
 setup(
     name="itecha",
@@ -11,7 +43,7 @@ setup(
     url="https://github.com/longern/itecha",
     license="MIT",
     packages=find_packages(),
-    package_data={"itecha": ["static/**/*"]},
+    package_data={"itecha": ["static/*", "static/*/*", "static/*/*/*"]},
     include_package_data=True,
     python_requires=">=3.6",
     install_requires=[
@@ -22,4 +54,8 @@ setup(
         "requests",
         "whitenoise",
     ],
+    extras_require={
+        "fc": ["djanble @ git+https://github.com/longern/djanble.git@main"]
+    },
+    cmdclass={"deploy": DeployCommand},
 )
