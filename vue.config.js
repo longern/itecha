c3aa5b2b3192118b28916b25d5db6e6fd6187022
Long <longsiyu@megvii.com>
2021-02-25 11:57:08 +0800

    Compress with gzip

--- vue.config.js
+++ vue.config.js
@@ -1,48 +1,64 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require('path');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  "outputDir": "itecha/static",
-  "transpileDependencies": [
-    "vuetify"
-  ],
+  outputDir: "itecha/static",
+  transpileDependencies: ["vuetify"],
   configureWebpack: {
     externals: {
-      "axios": "axios",
+      axios: "axios",
       "mavon-editor": "MavonEditor",
-      "vue": "Vue",
+      vue: "Vue",
       "vue-router": "VueRouter",
-      "vuetify": "Vuetify",
-      "vuetify-dialog": "VuetifyDialog"
-    }
-  }
+      vuetify: "Vuetify",
+      "vuetify-dialog": "VuetifyDialog",
+    },
+  },
+  pluginOptions: {
+    compression: {
+      gzip: {
+        filename: "[path].gz[query]",
+        algorithm: "gzip",
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
+      },
+    },
+  },
 };
 
 process.env.VUE_APP_API_BASE_URL = process.env.VUE_APP_API_BASE_URL || "/";
 
-process.env.VUE_APP_PYTHON3_EXECUTOR = process.env.VUE_APP_PYTHON3_EXECUTOR || "/python-executor";
+process.env.VUE_APP_PYTHON3_EXECUTOR =
+  process.env.VUE_APP_PYTHON3_EXECUTOR || "/python-executor";
 
 process.env.VUE_APP_ENABLE_CDN = process.env.VUE_APP_ENABLE_CDN || "";
 
 if (!process.env.VUE_APP_ENABLE_CDN)
   module.exports.configureWebpack.plugins = [
-    new CopyWebpackPlugin([{
-      from: 'node_modules/axios/dist',
-      to: path.resolve(module.exports.outputDir, 'axios'),
-    }, {
-      from: 'node_modules/vue/dist',
-      to: path.resolve(module.exports.outputDir, 'vue'),
-    }, {
-      from: 'node_modules/vue-router/dist',
-      to: path.resolve(module.exports.outputDir, 'vue-router'),
-    }, {
-      from: 'node_modules/vuetify/dist',
-      to: path.resolve(module.exports.outputDir, 'vuetify'),
-    }, {
-      from: 'node_modules/vuetify-dialog/dist',
-      to: path.resolve(module.exports.outputDir, 'vuetify-dialog'),
-    }, {
-      from: 'node_modules/mavon-editor/dist',
-      to: path.resolve(module.exports.outputDir, 'mavon-editor'),
-    }]),
+    new CopyWebpackPlugin([
+      {
+        from: "node_modules/axios/dist",
+        to: path.resolve(module.exports.outputDir, "axios"),
+      },
+      {
+        from: "node_modules/vue/dist",
+        to: path.resolve(module.exports.outputDir, "vue"),
+      },
+      {
+        from: "node_modules/vue-router/dist",
+        to: path.resolve(module.exports.outputDir, "vue-router"),
+      },
+      {
+        from: "node_modules/vuetify/dist",
+        to: path.resolve(module.exports.outputDir, "vuetify"),
+      },
+      {
+        from: "node_modules/vuetify-dialog/dist",
+        to: path.resolve(module.exports.outputDir, "vuetify-dialog"),
+      },
+      {
+        from: "node_modules/mavon-editor/dist",
+        to: path.resolve(module.exports.outputDir, "mavon-editor"),
+      },
+    ]),
   ];
