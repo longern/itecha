e41e3659cb4e3efa008b41aa7899617fb1c6d431
longern <i@longern.com>
2021-02-28 00:16:00 +0800

    Fix notification style

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import vuetify from "./plugins/vuetify";
-import VuetifyDialog from "vuetify-dialog";
 import mavonEditor from "mavon-editor";
 
 import App from "./App.vue";
@@ -10,11 +9,6 @@ import router from "./router.js";
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
-Vue.use(VuetifyDialog, {
-  context: {
-    vuetify,
-  },
-});
 Vue.use(mavonEditor);
 
 new Vue({
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,12 +1,23 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
+import VuetifyDialog from "vuetify-dialog";
 import zhHans from "vuetify/es5/locale/zh-Hans";
+
+import "./notify.css";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
+const vuetify = new Vuetify({
   lang: {
     locales: { zhHans },
     current: "zhHans",
   },
 });
+
+Vue.use(VuetifyDialog, {
+  context: {
+    vuetify,
+  },
+});
+
+export default vuetify;
