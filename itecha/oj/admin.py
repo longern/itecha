38022b75169326e53bcccb4fbd563044acc0a4fa
longern <771815005@qq.com>
2022-05-13 16:44:40 +0800

    Remove inner join

--- itecha/oj/admin.py
+++ itecha/oj/admin.py
@@ -14,10 +14,20 @@ class ContestAdmin(ImportExportMixin, admin.ModelAdmin):
 
 @admin.register(Problem)
 class ProblemAdmin(ImportExportMixin, admin.ModelAdmin):
-    pass
+    list_display = ("title", "created_at")
 
 
 @admin.register(Submission)
 class SubmissionAdmin(ImportExportMixin, admin.ModelAdmin):
-    list_display = ("problem", "creator", "score", "created")
+    list_display = ("problem_title", "creator_name", "score", "created")
     show_full_result_count = False
+
+    def problem_title(self, obj):
+        return getattr(obj.problem, "title", None)
+
+    def creator_name(self, obj):
+        return getattr(obj.creator, "username", None)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.prefetch_related("problem", "creator")
