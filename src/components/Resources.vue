60adad5f456602c5f7acf862d6d7075eaa881a09
longern <771815005@qq.com>
2022-05-20 18:36:58 +0800

    Add official docs

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -7,7 +7,7 @@
       <v-btn
         text
         href="https://v2.ykt.cbern.com.cn/65/document/94af91d903fe46959df0c5f86dd9bf1f/pdf.pdf"
-        download="数据与计算.pdf"
+        target="_blank"
       >
         数据与计算
       </v-btn>
@@ -16,7 +16,7 @@
       <v-btn
         text
         href="https://v2.ykt.cbern.com.cn/65/document/970cd78f6ce045609ca239d1ce096a5e/pdf.pdf"
-        download="信息系统与社会.pdf"
+        target="_blank"
       >
         信息系统与社会
       </v-btn>
@@ -25,7 +25,7 @@
       <v-btn
         text
         href="https://v2.ykt.cbern.com.cn/65/document/cd1d4ae30d324d25ad3ff62e8f1ac172/pdf.pdf"
-        download="数据与数据结构.pdf"
+        target="_blank"
       >
         数据与数据结构
       </v-btn>
@@ -39,6 +39,15 @@
         href="https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe"
       >
         下载Python 3.10.4
+      </v-btn>
+    </v-row>
+    <v-row>
+      <v-btn
+        text
+        href="https://docs.python.org/zh-cn/3/index.html"
+        target="_blank"
+      >
+        Python 官方文档
       </v-btn>
     </v-row>
   </v-container>
