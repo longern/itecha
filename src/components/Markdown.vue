998de2cc2f33b20500379d1822d55761f7547a8e
longern <771815005@qq.com>
2022-05-26 00:37:28 +0800

    Render ordered list as radio button

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,9 +1,10 @@
 <template>
   <!-- eslint-disable vue/no-v-html -->
   <div
+    ref="mc"
     class="markdown-body"
-    v-html="renderedContent"
   />
+  <!-- Class markdown-body is required by github-markdown-css -->
 </template>
 
 <script>
@@ -18,10 +19,43 @@ export default {
 
   props: { source: { type: String, default: "" } },
 
-  computed: {
-    renderedContent() {
-      return md.render(this.source || "");
-    }
-  }
+  watch: {
+    source() {
+      this.$refs.mc.innerHTML = md.render(this.source || "");
+
+      // Render ordered list with type=A as radio button
+      for (const ol of this.$refs.mc.querySelectorAll("ol[type=A]")) {
+        ol.setAttribute("value", 0);
+
+        ol.addEventListener("input", function (event) {
+          if (+this.getAttribute("value"))
+            this.children[this.getAttribute("value") - 1].classList.remove(
+              "checked"
+            );
+          this.setAttribute("value", event.detail);
+          this.children[event.detail - 1].classList.add("checked");
+        });
+
+        for (let liIndex = 0; liIndex < ol.children.length; liIndex++) {
+          const li = ol.children[liIndex];
+          li.addEventListener("click", function () {
+            this.parentElement.dispatchEvent(
+              new CustomEvent("input", { detail: +liIndex + 1 })
+            );
+          });
+        }
+      }
+    },
+  },
 };
 </script>
+
+<style>
+.markdown-body ol[type=A] > li {
+  border: 1px solid transparent;
+}
+
+.markdown-body ol[type=A] > li.checked {
+  border: 1px solid #4caf50;
+}
+</style>
